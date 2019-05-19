557aadfda66ec8025b4813ce94381e3d82e37351
lwairore <kwairore@gmail.com>
2019-05-20 00:36:31 +0300

    Import the main instance. Casll register_blueprint() method on application instance and pass in the blueprint.

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,10 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    # Registering main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     
     return app
 

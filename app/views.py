9223b45026a09e8a1017499a90917cb3496934a5
lwairore <kwairore@gmail.com>
2019-05-19 14:33:56 +0300

    Define news_headlines() function takes source as a parameter.Returns articels based on respective source provided.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-from .requests import get_all_news_sources as news_sources
+from .requests import get_all_news_sources
 
 @app.route('/')
 def index():
@@ -8,6 +8,15 @@ def index():
     This function views the root page that returns index.html page
     and its data.
     """
-    all_news_sources = news_sources()
+    all_news_sources = get_all_news_sources()
+    
     title = "Giko"
     return render_template("index.html", sources = all_news_sources, title = title)
+
+@app.route('/source/<source>')
+def news_healines(source):
+    """
+    This function retrieves live top and breaking headlines for a country.
+    """
+    return render_template('news_articles')
+

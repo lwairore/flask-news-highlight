0f13eb217ec48c38498241ded3471d41f40ae2d4
lwairore <kwairore@gmail.com>
2019-05-19 23:41:14 +0300

    Define search_articles() function that takes source as a parameter.

--- app/requests.py
+++ app/requests.py
@@ -1,13 +1,20 @@
 from app import app
 import urllib.request, json
-from .models import sources, headlines
+from .models import sources, headlines, everything, business_headlines
 
+# Classes
 Sources = sources.Sources
 Headlines = headlines.Headlines
+Everything = everything.Everything
+Business = business_headlines.Business
+
+# Keys
 api_key = app.config["NEWS_API_KEY"]
 sources_url = app.config['SOURCES_BASE_API_URL']
 everything_news_url = app.config['EVERYTHING_BASE_API_URL']
 top_headlines_news_url = app.config['TOP_HEADLINES_BASE_API_URL']
+business_top_headlines_url = app.config['BUSINESS_TOP_HEADLINES']
+
 
 
 def get_all_news_sources():
@@ -54,7 +61,7 @@ def process_all_news_sources_data(sources_list):
 
 def get_all_news_headlines(source):
     """
-    This function will retrieve top-headlines news and passing the\
+    This function will retrieve top-headlines news and passing the
     response it gets to process_all_headlines_data() function.
     """
     top_headlines_url = top_headlines_news_url.format(source, api_key )
@@ -63,11 +70,11 @@ def get_all_news_headlines(source):
         headline_data = url.read()
         headlines_response = json.loads(headline_data)
         headlines_results = None
-        if headlines_response['sources']:
+        if headlines_response['articles']:
             """
             Control flow filters out all empty sources.
             """
-            headlines_items = headlines_response['sources']
+            headlines_items = headlines_response['articles']
             headlines_results = process_all_headlines_data(headlines_items)
 
     return headlines_results
@@ -89,3 +96,93 @@ def process_all_headlines_data(headlines_list):
         headlines_processed_results.append(news_headlines)
 
     return headlines_processed_results
+
+def get_everything_news():
+    """
+    This function will retrieve everything type news and passing the
+    response it gets to process_all_everything_results() function.
+    """
+    everything_complete_url = everything_news_url.format(api_key)
+
+    with urllib.request.urlopen(everything_complete_url) as url:
+        everything_data = url.read()
+        everything_response = json.loads(everything_data)
+        everything_results = None
+
+        if everything_response['articles']:
+            everything_results_list = everything_response['articles']
+            everything_results = process_all_everything_results(everything_results_list)
+
+    return everything_results
+
+
+def process_all_everything_results(everything_results_list):
+    """
+    This function is responsible for converting a data given as per Everything class.
+    """
+    everything_results = []
+    for item in everything_results_list:
+        author = item.get('author')
+        title = item.get('title')
+        description = item.get('description')
+        url = item.get('url')
+        urlToImage = item.get('urlToImage')
+        publishedAt = item.get('publishedAt')
+
+        everything_object = Everything(author, title, description, url, urlToImage, publishedAt)
+        everything_results.append(everything_object)
+        
+    return everything_results
+
+def get_business_headlines():
+     """
+    This function will retrieves business_headlines type news and passing the
+    response it gets to process_all_everything_results() function.
+    """
+    business_headlines_complete_url = business_top_headlines_url.format(api_key)
+
+    with urllib.request.urlopen(business_headlines_complete_url) as url:
+        business_headlines_data = url.read()
+        business_headlines_response = json.loads(business_headlines_data)
+        business_headlines_results = None
+
+        if business_headlines_response['articles']:
+            business_headlines_results_list = business_headlines_response['articles']
+            business_headlines_results = process_all_business_headlines_results(business_headlines_results_list)
+
+    return business_headlines_results
+
+def process_all_business_headlines_results(business_headlines_results_list):
+    """
+    This function is responsible for converting a data given as per Business class.
+    """
+    business_headlines_results = []
+    for item in business_headlines_results_list:
+        author = item.get('author')
+        title = item.get('title')
+        description = item.get('description')
+        url = item.get('url')
+        urlToImage = item.get('urlToImage')
+        publishedAt = item.get('publishedAt')
+
+        business_headlines_object = Business(author, title, description, url, urlToImage, publishedAt)
+        business_headlines_results.append(business_headlines_object)
+        
+    return business_headlines_results
+
+def search_articles(source):
+    """
+    This function passes in api_key and source name.
+    Create the request and process the results.
+
+    """
+    search_article_url = 'https://newsapi.org/v2/everything?q={}&apiKey={}'.format(source, api_key)
+    with urllib.request.urlopen(search_article_url) as url:
+        search_article_data = url.read()
+        search_article_response = json.loads(search_article_data)
+        search_article_results = None
+        if search_article_response['articles']:
+            search_article_list = search_article_response['articles']
+            search_article_results = process_all_business_headlines_results(search_article_list)
+    return search_article_results
+        

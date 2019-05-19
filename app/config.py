991164d71987b946af970a6702ae3a70e2b945c2
lwairore <kwairore@gmail.com>
2019-05-19 09:35:49 +0300

    Create ProdConfig sub-class that inherits from Config, base-class, with pass function

--- app/config.py
+++ app/config.py
@@ -4,3 +4,12 @@ class Config:
     that will will be used in Development stage and Production class.
     """
     pass
+
+
+class ProdConfig(Config):
+    """
+    This sub-class will inherit all configurations from Config, base class.
+    This sub-class will also contain all the other configurations to
+    facilitate the production class. 
+    """
+    pass

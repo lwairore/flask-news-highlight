1ea0f61158e110137cabe3cac85dc8c21482f44d
lwairore <kwairore@gmail.com>
2019-05-19 11:24:48 +0300

    In sources module; Create a Sources class. Define __init__() method that takes id, name, url, country and description as parameters.

---
+++ app/models/sources.py
@@ -0,0 +1,19 @@
+# self.new_source = Sources("id", name, url, country, description)
+class Sources:
+    """
+    This class helps to design all its instance to have:
+        1. id
+        2. name
+        3. url
+        4. country
+        5. description
+    """
+    def __init__(self, id, name, url, country, description):
+        """
+        This method allows us to instantiate an instance.
+        """
+        self.id = id
+        self.name = name
+        self.url = url
+        self.country = country
+        self.description = description

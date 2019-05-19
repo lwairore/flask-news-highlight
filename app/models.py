3a5c2e37f9a411efbb4eb4ce481c2bdb759ff63c
lwairore <kwairore@gmail.com>
2019-05-20 00:40:29 +0300

    Add Sources class.

--- app/models.py
+++ app/models.py
@@ -65,7 +65,24 @@ class Headlines:
         self.urlToImage = urlToImage
         self.publishedAt = publishedAt
         
-
+class Sources:
+    """
+    This class helps to design all its instance to have:
+        1. id
+        2. name
+        3. url
+        4. country
+        5. description
+    """
+    def __init__(self, id, name, url, country, description):
+        """
+        This method allows us to instantiate an instance.
+        """
+        self.id = id
+        self.name = name
+        self.url = url
+        self.country = country
+        self.description = description
 
 
 

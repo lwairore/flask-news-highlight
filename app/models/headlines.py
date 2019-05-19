208db9b2b3b82b84413039281f0b73b414a9e545
lwairore <kwairore@gmail.com>
2019-05-19 23:58:52 +0300

    Rectify typos

---
+++ app/models/headlines.py
@@ -0,0 +1,22 @@
+class Headlines:
+    """
+    This class helps to design Headlines data to have:
+        1. author
+        2. title
+        3. url
+        4. urlToImage
+        5. description
+        6. publishedAt
+    """
+    
+    def __init__(self,author, title, description, url, urlToImage, publishedAt):
+        """
+        This method allows us to instantiate an instance.
+        """
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        

8083826cc7efdc30ebd9734724af21aba5bb41b4
lwairore <kwairore@gmail.com>
2019-05-20 00:48:55 +0300

    Import configure_request() function from the requests.py file.

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,11 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    
     return app
 
 # from app import views

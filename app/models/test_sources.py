37ebb5ff13162571345ff9327ef516ca0733725f
lwairore <kwairore@gmail.com>
2019-05-19 11:15:27 +0300

    In test_sources module; Define a setUp() method that instantiates new_sources instance.

--- app/models/test_sources.py
+++ app/models/test_sources.py
@@ -12,6 +12,15 @@ class TestSources(unittest.TestCase):
 
     """
 
+    def setUp(self):
+        """
+        Set up method to create a new_source instance
+        before each test cases.
+        """
+        # self.new_source = Sources("id", name, url, country, description)
+        self.new_source = Sources("ABC-Z", "News Cast", "https://www.news-cast.com ", "Kenya", "We provide in depth news from all around Kenya, with a 5 year experience..."  )
+
+    
 
 if __name__ == "__main__":
     unittest.main()

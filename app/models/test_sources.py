99641efa895a830265274cc55fdfe1a54abc93ea
lwairore <kwairore@gmail.com>
2019-05-19 11:19:26 +0300

    In test_sources module; Define test_instance() method that assertains if self.new_source instance is an instance of Sources class.

--- app/models/test_sources.py
+++ app/models/test_sources.py
@@ -20,7 +20,13 @@ class TestSources(unittest.TestCase):
         # self.new_source = Sources("id", name, url, country, description)
         self.new_source = Sources("ABC-Z", "News Cast", "https://www.news-cast.com ", "Kenya", "We provide in depth news from all around Kenya, with a 5 year experience..."  )
 
-    
+    def test_instance(self):
+        """
+        This method will only assert if self.new_source is an instance of
+        Sources class.
+        """
+        self.assertTrue(isinstance(self.new_source, Sources)) 
+
 
 if __name__ == "__main__":
     unittest.main()

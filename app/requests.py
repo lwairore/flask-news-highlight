a8e2c547f1287e10073aed177008927502fbfd1f
lwairore <kwairore@gmail.com>
2019-05-19 10:28:10 +0300

    Declare api_key variable that stores NEWS_API_KEY from app.config. Declare sources_url that stores SOURCES_BASE_API_URL from app.config. Define get_all_news_sources() function. Declared complete_sources_url variable that concatenates api_key to sources_url to form a complete sources url.

--- app/requests.py
+++ app/requests.py
@@ -1,1 +1,14 @@
 from app import app
+
+
+api_key = app.config["NEWS_API_KEY"]
+sources_url = app.config['SOURCES_BASE_API_URL']
+
+def get_all_news_sources():
+    """
+    This function will be responsible for fetching/requesting all the
+    news sources data. And the passing that data to be processed by
+    process_all_news_sources_data() function. 
+    get_all_news_sources() will finally return all the required news sources.
+    """
+    complete_sources_url = sources_url.format(api_key)

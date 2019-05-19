5afb36ec97ba3c3bcf223c0e861c7ff6d194c7b3
lwairore <kwairore@gmail.com>
2019-05-19 14:45:51 +0300

    Define get_all_news_headlines() function for sending a request to fetch top_headlines data.

--- app/requests.py
+++ app/requests.py
@@ -8,23 +8,6 @@ sources_url = app.config['SOURCES_BASE_API_URL']
 everything_news_url = app.config['EVERYTHING_BASE_API_URL']
 top_headlines_news_url = app.config['TOP_HEADLINES_BASE_API_URL']
 
-def process_all_news_sources_data(sources_list):
-    """
-    This function will process the sources response as per Sources class arguments;
-    Each source will be required to have an id, name, url, country, and description.
-    """
-    sources_processed_results = []
-    for item in sources_list:
-        id = item.get('id')
-        name = item.get('name')
-        url = item.get('url')
-        country = item.get('country')
-        description = item.get('description')
-        new_source = Sources(id, name, url, country, description)
-        sources_processed_results.append(new_source)
-
-    return sources_processed_results
-
 
 def get_all_news_sources():
     """
@@ -47,5 +30,49 @@ def get_all_news_sources():
             sources_results = process_all_news_sources_data(sources_items)
 
     return sources_results
+    
 
 
+def process_all_news_sources_data(sources_list):
+    """
+    This function will process the sources response as per Sources class arguments;
+    Each source will be required to have an id, name, url, country, and description.
+    """
+    sources_processed_results = []
+    for item in sources_list:
+        id = item.get('id')
+        name = item.get('name')
+        url = item.get('url')
+        country = item.get('country')
+        description = item.get('description')
+        new_source = Sources(id, name, url, country, description)
+        sources_processed_results.append(new_source)
+
+    return sources_processed_results
+
+
+def get_all_news_headlines(source):
+    """
+    This function will retrieve top-headlines news and passing the\
+    response it gets to process_all_headlines_data() function.
+    """
+    top_headlines_url = top_headlines_news_url.format(source, api_key )
+
+    with urllib.request.urlopen(top_headlines_url) as url:
+        headline_data = url.read()
+        headlines_response = json.loads(headline_data)
+        headlines_results = None
+        if headlines_response['sources']:
+            """
+            Control flow filters out all empty sources.
+            """
+            headlines_items = headlines_response['sources']
+            headlines_results = process_all_headlines_data(headlines_items)
+
+    return headlines_results
+
+
+def process_all_headlines_data(headlines_list):
+    """
+    This function is responsible for converting an data given as the Top-Headlines class.
+    """

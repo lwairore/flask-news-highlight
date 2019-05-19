535452ed57bb065ebb019fa0b4d663e9112c2f6d
lwairore <kwairore@gmail.com>
2019-05-19 14:01:35 +0300

    Declare everything_news_url to store EVERYTHING_BASE_API_URL and declare top_headlies_news_url to store TOP_HEADLINES_BASE_API_URL

--- app/requests.py
+++ app/requests.py
@@ -5,6 +5,26 @@ from .models import sources
 Sources = sources.Sources
 api_key = app.config["NEWS_API_KEY"]
 sources_url = app.config['SOURCES_BASE_API_URL']
+everything_news_url = app.config['EVERYTHING_BASE_API_URL']
+top_headlines_news_url = app.config['TOP_HEADLINES_BASE_API_URL']
+
+def process_all_news_sources_data(sources_list):
+    """
+    This function will process the sources response as per Sources class arguments;
+    Each source will be required to have an id, name, url, country, and description.
+    """
+    sources_processed_results = []
+    for item in sources_list:
+        id = item.get('id')
+        name = item.get('name')
+        url = item.get('url')
+        country = item.get('country')
+        description = item.get('description')
+        new_source = Sources(id, name, url, country, description)
+        sources_processed_results.append(new_source)
+
+    return sources_processed_results
+
 
 def get_all_news_sources():
     """
@@ -24,24 +44,8 @@ def get_all_news_sources():
             Control flow filters out all empty sources.
             """
             sources_items = sources_response['sources']
-            source_process_items = process_all_news_sources_data(sources_items)
+            sources_results = process_all_news_sources_data(sources_items)
 
     return sources_results
 
 
-def process_all_news_sources_data(sources_list):
-    """
-    This function will process the sources response as per Sources class arguments;
-    Each source will be required to have an id, name, url, country, and description.
-    """
-    sources_processed_results = []
-    for item in sources_list:import sourceimport source
-        id = item.get('id')
-        name = item.get('name')
-        url = item.get('url')
-        country = item.get('country')
-        description = item.get('description')
-        new_source = Sources(id, name, url, country, description)
-        sources_processed_results.append(new_source)
-
-    return sources_processed_results

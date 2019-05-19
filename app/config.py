6db21949e733608919eec415e94b796ce734db7f
lwairore <kwairore@gmail.com>
2019-05-19 09:32:47 +0300

    Create Config class with a pass function

---
+++ app/config.py
@@ -0,0 +1,6 @@
+class Config:
+    """
+    Config class will contain all(general) configurations/optimization
+    that will will be used in Development stage and Production class.
+    """
+    pass

2e0cb90e0068e2a6f2be4afde314132c8769edb4
lwairore <kwairore@gmail.com>
2019-05-19 10:16:18 +0300

    Extract api key from instance/config.py with from_pyfile() method and store it in app.config. Import views.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,8 @@ from .config import DevConfig
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+from app import views
 
 

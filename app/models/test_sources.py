171e3955d03a49c01600fcfe4d79c9192ca6593c
lwairore <kwairore@gmail.com>
2019-05-19 11:04:45 +0300

    In test_sources.py; Import unittest, sources module. Create TestSources() sub-class that inherits unittest.TestCase. Added an if control flow that only runs if test_sources.py file is run from its original state.

---
+++ app/models/test_sources.py
@@ -0,0 +1,17 @@
+import unittest
+import sources
+Sources = sources.Sources
+
+class TestSources(unittest.TestCase):
+    """
+    Text class that defines test cases 
+    for the sources class behaviours.
+    
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases.
+
+    """
+
+
+if __name__ == "__main__":
+    unittest.main()
